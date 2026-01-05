d72869390b7086f965526f6222f53927a8652c9f
Kleberson <106557254+klebersonLima@users.noreply.github.com>
2026-01-04 22:13:01 -0300

    Update pedidos.routes.js

--- routes/pedidos.routes.js
+++ routes/pedidos.routes.js
@@ -15,8 +15,8 @@ const router = express.Router();
 
 router.use(verificarToken);
 router.post('/orders', createOrder);        // inserir
-router.put('/orders/:id', updateOrder);     // atutlaizar
-router.put('/orders/item/:id', updateItem);     // atutlaizar item
+router.put('/orders/:id', updateOrder);     // atualizar
+router.put('/orders/item/:id', updateItem);     // atualizar item
 router.get('/orders/all', getAllOrders);        // lista todos
 router.get('/orders/:id', getOrderById);    // lista pelo pedido
 router.delete('/orders/:id', deleteOrder);  // deletar
